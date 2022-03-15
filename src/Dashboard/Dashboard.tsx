e628172e66a4ab621d8ecfa71033bc67094704c9
Tadas Baublys <taduks@gmail.com>
2022-03-15 21:20:59 +0200

    Small fixes

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -23,9 +23,7 @@ function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
+      {' iSafOssTool '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
